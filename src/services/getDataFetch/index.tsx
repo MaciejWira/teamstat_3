01db26c0ef983f18a3562024c32a70f684c26bff
maciej-wira-tebra <130485354+maciej-wira-tebra@users.noreply.github.com>
2023-12-16 21:35:57 +0100

    POST queries

--- src/services/getDataFetch/index.tsx
+++ src/services/getDataFetch/index.tsx
@@ -25,37 +25,55 @@ export const getDataFetch = async <TResult, TVariables>({
 }): Promise<TResult> => {
   // first try GET, then POST
 
-  try {
-    const responseParams = await fetch(
-      `${wpgqlPath}?${new URLSearchParams({
-        query: print(document),
-        variables: variables ? JSON.stringify(variables) : "",
-      })}`,
-      fetchOptions(tags)
+  // try {
+  //   const responseParams = await fetch(
+  //     `${wpgqlPath}?${new URLSearchParams({
+  //       query: print(document),
+  //       variables: variables ? JSON.stringify(variables) : "",
+  //     })}`,
+  //     fetchOptions(tags)
+  //   );
+  //   const jsonParams = await responseParams.json();
+
+  //   if (jsonParams.errors) throw new Error("Error");
+  //   return jsonParams.data;
+  // } catch {
+  //   const response = await fetch(`${wpgqlPath}/`, {
+  //     method: "POST",
+  //     body: JSON.stringify({
+  //       query: print(document),
+  //       variables,
+  //     }),
+  //     ...fetchOptions(tags),
+  //   });
+
+  //   if (response.status !== 200) {
+  //     throw new Error(
+  //       `Failed to fetch: ${
+  //         response.statusText
+  //       }. Body: ${await response.text()}`
+  //     );
+  //   }
+
+  //   const json = await response.json();
+  //   return json.data;
+  // }
+
+  const response = await fetch(`${wpgqlPath}/`, {
+    method: "POST",
+    body: JSON.stringify({
+      query: print(document),
+      variables,
+    }),
+    ...fetchOptions(tags),
+  });
+
+  if (response.status !== 200) {
+    throw new Error(
+      `Failed to fetch: ${response.statusText}. Body: ${await response.text()}`
     );
-    const jsonParams = await responseParams.json();
-
-    if (jsonParams.errors) throw new Error("Error");
-    return jsonParams.data;
-  } catch {
-    const response = await fetch(`${wpgqlPath}/`, {
-      method: "POST",
-      body: JSON.stringify({
-        query: print(document),
-        variables,
-      }),
-      ...fetchOptions(tags),
-    });
-
-    if (response.status !== 200) {
-      throw new Error(
-        `Failed to fetch: ${
-          response.statusText
-        }. Body: ${await response.text()}`
-      );
-    }
-
-    const json = await response.json();
-    return json.data;
   }
+
+  const json = await response.json();
+  return json.data;
 };
